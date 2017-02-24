5fde054d4104af7158f508e0cd318245f4bb2fe6
Raydelto <raydelto@gmail.com>
2017-02-24 00:09:27 -0400

    Game totally functional

--- src/app.js
+++ src/app.js
@@ -1,24 +1,120 @@
 
 var TicTacToeLayer = cc.LayerColor.extend({
     board:null,
-    xIsPlaying: true,
+    size:null,
+    xIsPlaying: true,    
+    gameOver: false,
     boardData:[[-1,-1,-1],
                [-1,-1,-1],
                [-1,-1,-1]],
     cellLocation:[[{x:113,y:360},{x:226,y:360},{x:360,y:360}],
                   [{x:113,y:240},{x:226,y:240},{x:360,y:240}],
                   [{x:113,y:113},{x:226,y:113},{x:360,y:113}]],
-    verifyWinner: function(){
+    endGameWithText: function(text){
+        var label = new cc.LabelTTF(text, "Arial", 38);
+        label.setPosition( this.size.width / 2, this.size.height / 2);
+        label.setColor(cc.color(255,255,51));
+        this.addChild(label, 5);      
+    },
+    showWinner: function(){
+        var winner = this.verifyWinner();
+        if(winner === -1 && this.verifyTie()){
+            cc.log('Tie');
+            this.endGameWithText('Tie');
+            this.gameOver = true;
+            return;
+        }
+        switch(winner){
+            case 0:
+                cc.log('O won the match');
+                this.endGameWithText('O won the match');
+                this.gameOver = true;
+                break;
+            case 1:
+                cc.log('X won the match');
+                this.endGameWithText('X won the match');
+                this.gameOver = true;
+                break;
+        }    
+    },
+    verifyTie: function(){      
+        for(var i = 0 ; i < 3 ; i++){
+            for(var j = 0 ; j < 3 ; j++){
+                if(this.boardData[i][j] === -1 ){
+                    return false;
+                }
+            }
+        }
+        return true;        
+    },
+    verifyWinner: function(){        
+        var play = -1;
+        //horizontal verification
+        for(var i = 2 ; i >= 0 ; i--){
+            play = this.boardData[i][0];
+            for(var j = 1 ; j < 3 ; j++){                
+                if(this.boardData[i][j] === -1 || this.boardData[i][j] !== play){
+                    play = -1;
+                    break;                    
+                }                
+                play = this.boardData[i][j];                
+            }
+            if(play != -1){
+                return play;
+            }
+        }
 
+        //vertical verification
+        for(var i = 2 ; i >= 0 ; i--){
+            play = this.boardData[i][0];
+            for(var j = 1 ; j < 3 ; j++){                
+                if(this.boardData[j][i] === -1 || this.boardData[j][i] !== play){
+                    play = -1;
+                    break;                    
+                }                
+                play = this.boardData[j][i];                
+            }
+            if(play != -1){
+                return play;
+            }
+        }                    
+
+        //diagonal verification (left to right)
+        play = this.boardData[0][0];
+        for(var j = 1 ; j < 3 ; j++){                
+            if(this.boardData[j][j] === -1 || this.boardData[j][j] !== play){
+                play = -1;
+                break;                    
+            }                
+            play = this.boardData[j][j];                
+        }
+        if(play != -1){
+            return play;
+        }               
+
+        //diagonal verification (right to left)
+        play = this.boardData[0][2];
+        for(var j = 1 ; j >=0 ; j--){                
+            if(this.boardData[2 - j][j] === -1 || this.boardData[2 - j][j] !== play){
+                play = -1;
+                break;                    
+            }                
+            play = this.boardData[2 - j][j];                
+        }
+        if(play != -1){
+            return play;
+        }       
+        return play;
     },
     handleTouch:function(touch, event){
         var point = touch.getLocation();
         var self = event.getCurrentTarget();
         cc.log(point);
         var cell = self.getCell(point);
-        if(self.boardData[cell.i][cell.j] === -1){
+        if(!self.gameOver && self.boardData[cell.i][cell.j] === -1){
             self.boardData[cell.i][cell.j] = self.xIsPlaying ? 1 : 0;
             self.drawPlay(cell);
+            self.showWinner();
         }        
     },
     getCell:function (point){
@@ -56,9 +152,9 @@ var TicTacToeLayer = cc.LayerColor.extend({
     ctor:function () {
         this._super();
         this.init( cc.color(0,128,255,255) );
-        var size = cc.winSize;
+        this.size = cc.winSize;
         this.board = new cc.Sprite(res.board);
-        this.board.setPosition( size.width / 2, size.height / 2);
+        this.board.setPosition( this.size.width / 2, this.size.height / 2);
         this.addChild(this.board, 0); 
         this.touchInit();
         return true;
